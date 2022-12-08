746cf65f95f4f10031aec761acc1099f05029f33
Tracie35 <kariukitracie@gmail.com>
2022-12-08 08:30:32 +0300

    working on the login

--- book-review/src/pages/Login/Login.jsx
+++ book-review/src/pages/Login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import "./Login.css";
- 
-const Login =  () =>{
-    return (<div className='cover'>
-        <h1>login</h1>
-    </div>)
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+function Login() {
+  return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 w-[100%] mt-4'>
+        <div className='hidden sm:block'>
+            <img className='w-full h-full object-cover' src="https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        </div>
+
+        <div className='bg-gray-800 flex flex-col justify-center'>
+            <form className='max-w-[400px] w-full mx-auto rounded-lg bg-gray-900 p-8 px-8'>
+                <h2 className='text-4xl dark:text-white font-bold text-center'>SIGN IN</h2>
+                <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Username</label>
+                    <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
+                </div>
+                <div className='flex flex-col text-gray-400 py-2'>
+                    <label>Password</label>
+                    <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" />
+                </div>
+                <div className='flex justify-between text-gray-400 py-2'>
+                    <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
+                    <p>Forgot Password</p>
+                </div>
+                <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>SIGN IN</button>
+                <p>Don't have an account? <Link to='/signup'>Create account</Link></p>
+            </form>
+        </div>
+    </div>
+  )
 }
+
 export default Login
